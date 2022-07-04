ff9faa3704f7102787f80cfef812eed2abd1ed74
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-04 17:32:10 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tailwind-angular-v3';
+  mostrarMensaje() {
+    alert('Hola mundo');
+  }
 }
