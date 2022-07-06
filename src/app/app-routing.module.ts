2e44747e28ad8f84664fe62e58b82fbc8220a752
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-05 19:59:10 -0500

    routes for login and error 404 pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './modules/errors/page-not-found/page-not-found.component';
+import { HomeComponent } from './modules/main/home/home.component';
+import { LoginComponent } from './modules/security/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
