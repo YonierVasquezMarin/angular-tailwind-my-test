3e9b72e30c6e6bb020a22be1f4d9fe9d196188e3
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-06 16:57:18 -0500

    auth guards for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth/auth.guard';
 import { DashboardComponent } from './modules/dashboard/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './modules/errors/page-not-found/page-not-found.component';
 import { HomeComponent } from './modules/main/home/home.component';
 import { LoginComponent } from './modules/security/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '', component: HomeComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: '**', component: PageNotFoundComponent}
 ];
 
