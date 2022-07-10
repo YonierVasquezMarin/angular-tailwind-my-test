e54dff883dfd1e139f90d9b6f7c87561e92e208b
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-09 21:12:45 -0500

    login guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth/auth.guard';
+import { LoginsGuard } from './guards/login/login.guard';
 import { DashboardComponent } from './modules/dashboard/dashboard/dashboard.component';
 import { PageNotFoundComponent } from './modules/errors/page-not-found/page-not-found.component';
 import { HomeComponent } from './modules/main/home/home.component';
@@ -8,7 +9,7 @@ import { LoginComponent } from './modules/security/login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [LoginsGuard] },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'proof-of-concept', loadChildren: () => import('./modules/proof-of-concept/proof-of-concept.module').then(m => m.ProofOfConceptModule) },
   { path: '**', component: PageNotFoundComponent }
