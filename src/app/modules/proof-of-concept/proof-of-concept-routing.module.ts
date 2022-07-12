e98c999b07ed33019468d066e7c08e06ed53ef4a
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2022-07-11 19:16:21 -0500

    some tailwind elements added

--- src/app/modules/proof-of-concept/proof-of-concept-routing.module.ts
+++ src/app/modules/proof-of-concept/proof-of-concept-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PipeComponent } from './pipe/pipe.component';
 import { StylesScssComponent } from './styles-scss/styles-scss.component';
+import { TailwindcssComponent } from './tailwindcss/tailwindcss.component';
 import { TestingHomePageComponent } from './testing-home-page/testing-home-page.component';
 
 const routes: Routes = [
   { path: '', component: TestingHomePageComponent },
   { path: 'pipes', component: PipeComponent },
   { path: 'scss', component: StylesScssComponent },
+  { path: 'tailwindcss', component: TailwindcssComponent }
 ];
 
 @NgModule({
--- src/app/modules/proof-of-concept/proof-of-concept.module.ts
+++ src/app/modules/proof-of-concept/proof-of-concept.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { TestingHomePageComponent } from './testing-home-page/testing-home-page.component';
 import { ProofOfConceptRoutingModule } from './proof-of-concept-routing.module';
 import { StylesScssComponent } from './styles-scss/styles-scss.component';
+import { TailwindcssComponent } from './tailwindcss/tailwindcss.component';
 
 @NgModule({
   declarations: [
     TestingHomePageComponent,
-    StylesScssComponent
+    StylesScssComponent,
+    TailwindcssComponent
   ],
   imports: [
     CommonModule,
